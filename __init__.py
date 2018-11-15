54c80c1ad8f0d7f58c171d1973300babc8392fae
Smo <smoerble@smoerble.de>
2018-11-15 11:14:09 +0100

    ver 1 :wq

--- __init__.py
+++ __init__.py
@@ -20,42 +20,42 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
-__author__ = 'eward'
+__author__ = 'smo based on eward'
 
 LOGGER = getLogger(__name__)
 
 
-class HelloWorldSkill(MycroftSkill):
+class HelloVodafoneSkill(MycroftSkill):
     def __init__(self):
-        super(HelloWorldSkill, self).__init__(name="HelloWorldSkill")
+        super(HelloVodafoneSkill, self).__init__(name="HelloVodafoneSkill")
 
     def initialize(self):
-        thank_you_intent = IntentBuilder("ThankYouIntent"). \
-            require("ThankYouKeyword").build()
-        self.register_intent(thank_you_intent, self.handle_thank_you_intent)
+        #thank_you_intent = IntentBuilder("ThankYouIntent"). \
+        #    require("ThankYouKeyword").build()
+        #self.register_intent(thank_you_intent, self.handle_thank_you_intent)
 
-        how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
-            require("HowAreYouKeyword").build()
-        self.register_intent(how_are_you_intent,
-                             self.handle_how_are_you_intent)
+        #how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
+        #    require("HowAreYouKeyword").build()
+        #self.register_intent(how_are_you_intent,
+        #                     self.handle_how_are_you_intent)
 
-        hello_world_intent = IntentBuilder("HelloWorldIntent"). \
-            require("HelloWorldKeyword").build()
-        self.register_intent(hello_world_intent,
-                             self.handle_hello_world_intent)
+        hello_vodfane_intent = IntentBuilder("HelloVodafoneIntent"). \
+            require("HelloVodafoneKeyword").build()
+        self.register_intent(hello_vodafone_intent,
+                             self.handle_hello_vodafone_intent)
 
-    def handle_thank_you_intent(self, message):
-        self.speak_dialog("welcome")
+    #def handle_thank_you_intent(self, message):
+    #    self.speak_dialog("welcome")
 
-    def handle_how_are_you_intent(self, message):
-        self.speak_dialog("how.are.you")
+    #def handle_how_are_you_intent(self, message):
+    #    self.speak_dialog("how.are.you")
 
     def handle_hello_world_intent(self, message):
-        self.speak_dialog("hello.world")
+        self.speak_dialog("hello.vodafone")
 
     def stop(self):
         pass
 
 
 def create_skill():
-    return HelloWorldSkill()
+    return HelloVodafoneSkill()
